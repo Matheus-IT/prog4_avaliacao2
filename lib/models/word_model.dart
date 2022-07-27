9fd34fefd94ac113cd5622b8786221a4487265f7
matheus costa <matheuscosta.tech@gmail.com>
2022-07-27 12:23:01 -0300

    Model for word

---
+++ lib/models/word_model.dart
@@ -0,0 +1,24 @@
+import 'package:prog4_avaliacao2/models/letter_model.dart';
+
+class WordModel {
+  late List<LetterModel> _letters;
+
+  WordModel(String word) {
+    _letters = _createLetters(word);
+  }
+
+  List<LetterModel> get letters => [..._letters];
+
+  List<LetterModel> _createLetters(String word) {
+    return word
+        .split('')
+        .map<LetterModel>((letter) => LetterModel(letter.toUpperCase()))
+        .toList();
+  }
+
+  bool contains(String letter) {
+    return _letters.any((letterModel) => letterModel.value == letter);
+  }
+
+  int get length => _letters.length;
+}
